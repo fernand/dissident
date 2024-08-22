5279863b1e9a4ba5533ed02e1346bdbc0cffa6b2
Fernand Pajot <fernand@epigram.me>
2024-08-22 15:00:58 -0600

    .

--- get_ceo_history.py
+++ get_ceo_history.py
@@ -1,5 +1,6 @@
 import json
 
+import lxml.html
 import httpx
 
 import utils
@@ -15,7 +16,9 @@ def get_8k_urls(cik, cumul_urls=[]):
         for hit in current_hits:
             f1, f2 = hit['_id'].split(':')
             f1 = f1.replace('-', '')
-            cumul_urls.append('/'.join(('https://www.sec.gov/Archives/edgar/data/1633917', f1, f2)))
+            url = '/'.join(('https://www.sec.gov/Archives/edgar/data/1633917', f1, f2))
+            has_502 = '5.02' in hit['items']
+            cumul_urls.append((url, has_502))
         if total_hits - len(cumul_urls) > 0:
             return get_8k_urls(cik, cumul_urls)
         else:
@@ -24,4 +27,23 @@ def get_8k_urls(cik, cumul_urls=[]):
         print(f"Error: Unable to fetch data. {response.status_code} {response.text}")
         return []
 
-data = get_8k_urls('0001633917')
+def get_text(node):
+    text = ''
+    if node.text is not None:
+        text += node.text
+    text += '\n'.join([get_text(child) for child in node])
+    if node.tail:
+        text += node.tail
+    return text
+
+@utils.rate_limiter(calls_per_second=10)
+def get_8k(url):
+    url = 'https://www.sec.gov/Archives/edgar/data/1633917/000163391716000222/a8-kready.htm'
+    response = httpx.get(url, headers={'User-Agent': 'Your Name (your.email@example.com)'})
+    html_content = response.text.replace('&nbsp;', ' ')
+    tree = lxml.html.fromstring(html_content)
+    return get_text(tree)
+
+if __name__ == '__main__':
+    # data = get_8k_urls('0001633917')
+    pass
