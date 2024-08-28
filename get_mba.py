da31205b69e52d917633824bf41a43cb06b1b2f4
Fernand Pajot <fernand@epigram.me>
2024-08-28 11:26:36 -0600

    .

--- get_mba.py
+++ get_mba.py
@@ -22,23 +22,11 @@ class MBAResult:
     ceo_response:str = None
     ceo_mba_response:str = None
 
-def ceo_question(company_name, symbol):
-    return f"Who is the CEO of {company_name} (stock ticker {symbol})"
-
 def ceo_mba_question(ceo_name, company_name):
     return f"Does {ceo_name}, CEO of {company_name} have an MBA or MBA like degree?"
 
-def mba_query(openai_client, perplexity_client, company_name, symbol):
-    ceo_response = utils.get_perplexity_response(perplexity_client, ceo_question(company_name, symbol))
-    print(ceo_response)
-    ceo_name = utils.get_openai_response(
-        openai_client,
-        "Extract the name of the CEO.",
-        ceo_response,
-        CEOName,
-    ).name
-    print(ceo_name)
-    ceo_mba_response = utils.get_perplexity_response(perplexity_client, ceo_mba_question(ceo_name, company_name))
+def mba_query(openai_client, perplexity_client, company):
+    ceo_mba_response = utils.get_perplexity_response(perplexity_client, ceo_mba_question(company['ceo_name'], company['name']))
     print(ceo_mba_response)
     ceo_has_mba = utils.get_openai_response(
         openai_client,
@@ -49,18 +37,16 @@ def mba_query(openai_client, perplexity_client, company_name, symbol):
     print(ceo_has_mba)
     print()
     return MBAResult(
-        symbol,
-        company_name,
-        ceo_name,
+        company['symbol'],
+        company['name'],
+        company['ceo_name'],
         ceo_has_mba,
-        ceo_response,
         ceo_mba_response,
     )
 
 if __name__ == '__main__':
     results_path = 'results_mba.pkl'
     companies = utils.get_nasdaq_companies()
-
     def query(company):
-        return mba_query(utils.openai_client, utils.perplexity_client, company['name'], company['symbol'])
+        return mba_query(utils.openai_client, utils.perplexity_client, company)
     utils.continue_doing(mba_query)
