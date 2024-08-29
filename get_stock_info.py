21d1c0755b97cdfe3fda425de535d20d694c226a
Fernand Pajot <fernand@epigram.me>
2024-08-29 11:40:01 -0600

    .

--- get_stock_info.py
+++ get_stock_info.py
@@ -32,23 +32,26 @@ class TickerInfo:
     active: bool
     market_cap: Optional[int]
 
-async def get_ticker_info(client, semaphore, ticker):
+async def get_ticker_info(client, semaphore, ticker, date):
     async with semaphore:
-        resp = await client.get(f'https://api.polygon.io/v3/reference/tickers/{ticker}?{API_KEY}')
-    results = resp.json()['results']
+        resp = await client.get(f'https://api.polygon.io/v3/reference/tickers/{ticker}?date={date}&{API_KEY}')
+    resp = resp.json()
+    if 'results' not in resp:
+        return None
+    results = resp['results']
     return TickerInfo(
         results['ticker'],
         results['primary_exchange'],
         results['cik'] if 'cik' in results else None,
-        results['type'],
+        results['type'] if 'type' in results else None,
         results['active'],
         results['market_cap'] if 'market_cap' in results else None,
     )
 
-async def get_all_ticker_info(tickers, num_concurrent=10):
+async def get_all_ticker_info(tickers, date, num_concurrent=10):
     semaphore = asyncio.Semaphore(num_concurrent)
     async with httpx.AsyncClient() as client:
-        tasks = [get_ticker_info(client, semaphore, ticker) for ticker in tickers]
+        tasks = [get_ticker_info(client, semaphore, ticker, date) for ticker in tickers]
         results = await tqdm_asyncio.gather(*tasks, total=len(tasks))
     return results
 
@@ -72,8 +75,8 @@ async def get_historical_data(ticker, dates, num_concurrent=10):
 def step_1_get_tickers(date):
     resp = httpx.get(f'https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/{date}?adjusted=true&{API_KEY}').json()
     tickers = [r['T'] for r in resp['results']]
-    results = asyncio.run(get_all_ticker_info(tickers))
-    with open('results_ticker_info.pkl', 'wb') as f:
+    results = asyncio.run(get_all_ticker_info(tickers, date))
+    with open(f'results_ticker_info_{date}.pkl', 'wb') as f:
         pickle.dump(results, f)
 
 def step_2_get_top_500():
@@ -90,8 +93,25 @@ def step_3_get_historical_data(tickers, start_date, end_date):
     with open('results_n100_historical.pkl', 'wb') as f:
         pickle.dump(results, f)
 
+def step_5_compare_performance():
+    pass
+
 if __name__ == '__main__':
-    # step_1_get_tickers('2024-08-27')
+    start_dt, end_dt = '2022-08-29', '2024-08-27'
+    # step_1_get_tickers(start_dt)
+    # step_1_get_tickers(end_dt)
     # step_2_get_top_500()
-    step_3_get_historical_data(n100.N100, '2019-8-29', '2024-08-27')
-    # changes = {t: c for t, c in changes.items() if t in n100.N100}
+    # step_3_get_historical_data(n100.N100, '2019-8-29', '2024-08-27')
+
+    from get_mba import MBAResult
+    with open('results_n100_historical.pkl', 'rb') as f:
+        historical_close: dict[str, tuple[str, float]] = pickle.load(f)
+    with open(f'results_ticker_info_{end_dt}.pkl', 'rb') as f:
+        start_ticker_info: dict[str, TickerInfo] = pickle.load(f)
+        start_ticker_info = {tinfo.ticker: tinfo for tinfo in start_ticker_info if tinfo is not None and tinfo.ticker in n100.N100}
+    with open(f'results_ticker_info_{start_dt}.pkl', 'rb') as f:
+        end_ticker_info: dict[str, TickerInfo] = pickle.load(f)
+        end_ticker_info = {tinfo.ticker: tinfo for tinfo in end_ticker_info if tinfo is not None and tinfo.ticker in n100.N100}
+    with open('results_mba_n100.pkl', 'rb') as f:
+        mba_results = pickle.load(f)
+
