edeb63287873a096cf34e8fec2c3c8062943b2cd
Fernand Pajot <fernand@epigram.me>
2024-09-19 08:48:10 -0600

    .

---
+++ create_fund.py
@@ -0,0 +1,13 @@
+import pickle
+
+from get_historical_data import TickerInfo
+
+if __name__ == '__main__':
+    # with open('historical_data.pkl', 'rb') as f:
+    #     data = pickle.load(f)
+    blacklist = ['CSGP']
+    sorted_data = sorted(
+        data['2019-09-11'],
+        key=lambda info: info.market_cap if info.market_cap is not None and info.ticker not in blacklist else 0,
+        reverse=True,
+    )
