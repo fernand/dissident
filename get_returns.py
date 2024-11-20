1d068f8aba2c30d44a58610e76ffe1c463bf2051
Fernand Pajot <fernand@epigram.me>
2024-11-20 12:20:01 -0700

    Bug fix.

--- get_returns.py
+++ get_returns.py
@@ -6,12 +6,10 @@ from classes import TickerInfo
 def portfolio_returns(portfolio: dict[str, float], start_data: list[TickerInfo], end_data: list[TickerInfo]):
     start_data = dict([(info.ticker, info) for info in start_data])
     end_data = dict([(info.ticker, info) for info in end_data])
-    start_value = 0
-    end_value = 0
-    for ticker, qty in portfolio.items():
-        start_value += qty * start_data[ticker].close
-        end_value += qty * end_data[ticker].close
-    returns = 100 * (end_value - start_value) / start_value
+    returns = 0
+    for ticker, weight in portfolio.items():
+        stock_return = (end_data[ticker].close - start_data[ticker].close) / start_data[ticker].close
+        returns += 100 * weight * stock_return
     print(f'Returns: {returns:.1f}%')
 
 if __name__ == '__main__':
